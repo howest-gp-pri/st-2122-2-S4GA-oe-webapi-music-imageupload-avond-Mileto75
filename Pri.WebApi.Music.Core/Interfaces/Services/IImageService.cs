2faa8038172cc789548b417749da3968f24c6fb7
Mileto Di Marco <mileto.di.marco@howest.be>
2022-03-28 19:05:53 +0200

    Add IImageService implementation

---
+++ Pri.WebApi.Music.Core/Interfaces/Services/IImageService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.WebApi.Music.Core.Interfaces.Services
+{
+    public interface IImageService
+    {
+        Task<string> AddImageAsync(IFormFile image);
+        Task<string> UpdateImageAsync(IFormFile image,string filename);
+        string GetUrl(string filename);
+
+    }
+}
---
+++ Pri.WebApi.Music.Core/Services/ImageService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Pri.WebApi.Music.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.WebApi.Music.Core.Services
+{
+    public class ImageService : IImageService
+    {
+        public async Task<string> AddImageAsync(IFormFile image)
+        {
+            
+        }
+
+        public string GetUrl(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UpdateImageAsync(IFormFile image, string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
